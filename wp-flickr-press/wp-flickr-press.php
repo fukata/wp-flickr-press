8b8690db873856a83977b10f3510150ce63ab34a
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2010-11-13 00:49:05 +0800

    add plugin information

---
+++ wp-flickr-press/wp-flickr-press.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: wp-flickr-press
+Plugin URI: 
+Description: Flickr integration for wordpress plugin.
+Version: 0.0.1
+Author: Tatsuya Fukata
+Author URI: http://fukata.org
+*/
+?>
