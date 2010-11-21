803b99d48ecce21105e94920f5ad2b25be062c75
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2010-11-21 18:57:00 +0800

    add match insert into post.

--- media-send-editor.php
+++ media-send-editor.php
@@ -2,37 +2,49 @@
 if ( isset($_POST['send']) && isset($_POST['attachments']) ) {
 	$keys = array_keys($_POST['send']);
 	$send_id = array_shift($keys);
-	$attachment = $_POST['attachments'][$send_id];
+	$attachments = array($send_id => $_POST['attachments'][$send_id]);
+} else if ( isset($_POST['batch']) && $_POST['batch'] && isset($_POST['batch_send']) && count($_POST['batch_send'])>0 && isset($_POST['attachments']) ) {
+	$attachments = array();
+	foreach ($_POST['batch_send'] as $id) {
+		if (isset($_POST['attachments'][$id])) {
+			$attachments[$id] = $_POST['attachments'][$id];
+		}
+	}
 } else {
 	wp_die('does not exists key.');
 }
 
-$photo = FlickrPress::getClient()->photos_getInfo($send_id);
+//$photo = FlickrPress::getClient()->photos_getInfo($send_id);
 
-media_send_to_editor(fp_create_image_html($photo, $attachment));
+media_send_to_editor(fp_create_image_html($attachments));
 
-function fp_create_image_html($photo, $attachment) {
-
-	$link = esc_url($attachment['url']);
-	$target = isset($attachment['target']) ? esc_attr($attachment['target']) : '';
-	$target = strlen($target)>0 ? " target='{$target}'" : '';
-	$align = isset($attachment['align']) ? esc_attr($attachment['align']) : '';
-	$src = isset($attachment['image-size']) ? esc_attr($attachment['image-size']) : '';
-	$alt = isset($attachment['title']) ? esc_attr($attachment['title']) : '';
-	if (strlen($src)==0) {
-		$src = FlickrPress::getPhotoUrl($photo);
-	}
-	if ( isset($attachment['align']) ) {
-		$align = esc_attr($attachment['align']);
-		$class = " class='align$align'";
-	}
-
+function fp_create_image_html($attachments) {
 
 	$html = '';
-	$html = "<img src='{$src}' alt='{$alt}'{$class} />";
-	if (strlen($link)>0) {
-		$html = "<a href='{$link}'{$target}>{$html}</a>";
+	foreach ($attachments as $id => $attachment) {
+		$photo = FlickrPress::getClient()->photos_getInfo($id);
+
+	        $link = esc_url($attachment['url']);
+	        $target = isset($attachment['target']) ? esc_attr($attachment['target']) : '';
+	        $target = strlen($target)>0 ? " target='{$target}'" : '';
+	        $align = isset($attachment['align']) ? esc_attr($attachment['align']) : '';
+	        $src = isset($attachment['image-size']) ? esc_attr($attachment['image-size']) : '';
+	        $alt = isset($attachment['title']) ? esc_attr($attachment['title']) : '';
+	        if (strlen($src)==0) {
+	                $src = FlickrPress::getPhotoUrl($photo);
+	        }
+	        if ( isset($attachment['align']) ) {
+	                $align = esc_attr($attachment['align']);
+	                $class = " class='align$align'";
+	        }
+
+		$_html = "<img src='{$src}' alt='{$alt}'{$class} />";
+		if (strlen($link)>0) {
+			$_html = "<a href='{$link}'{$target}>{$_html}</a>";
+		}
+		$html .= $_html;
 	}
+
 	return $html;
 }
 ?>
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://fukata.org/
 Tags: images,flickr
 Requires at least: 3.0.1
 Tested up to: 3.0.1
-Stable tag: 0.0.7
+Stable tag: 0.1.0
 
 Flickr画像を記事本文内に挿入する。
 
@@ -37,6 +37,9 @@ PHP依存ライブラリ
 4. Setting config
 
 == Changelog ==
+= 0.1.0 =
+* 一括挿入機能を追加
+
 = 0.0.7 =
 * タグサジェスト機能を追加
 
