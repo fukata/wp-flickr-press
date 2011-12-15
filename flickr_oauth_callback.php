d802bc9d43f9dfccd3aa3e6d9779000424e27510
Tatsuya Fukata <tatsuya.fukata@gmail.com>
2011-12-16 01:33:57 +0900

    bug fixed use both flickr-gallery plugin.

--- flickr_oauth_callback.php
+++ flickr_oauth_callback.php
@@ -6,5 +6,5 @@ require_once(dirname(__FILE__).'/../../../wp-admin/admin.php');
 require_once(dirname(__FILE__).'/FlickrPress.php');
 require_once(dirname(__FILE__).'/check-oauth.php');
 
-$token = FlickrPress::getClient()->auth_getToken(@$_GET['frob']);
+$token = FlickrPress::getClient()->auth_getToken($frob);
 include_once(dirname(__FILE__).'/inc.flickr_oauth_callback.php');
