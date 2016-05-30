562faa55ac8cf327af5131339ec3e112baf7a294
fukata <tatsuya.fukata@gmail.com>
2016-05-30 23:42:55 +0700

    fix plugin url and author url

--- wp-flickr-press.php
+++ wp-flickr-press.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: wp-flickr-press
-Plugin URI: http://fukata.org/dev/wp-plugin/wp-flickr-press/
+Plugin URI: https://github.com/fukata/wp-flickr-press
 Description: Flickr integration for wordpress plugin.
 Version: 2.3.1
 Author: Tatsuya Fukata, Alexander Ovsov
-Author URI: http://fukata.org
+Author URI: https://fukata.org
 */
 
 require_once(dirname(__FILE__).'/FlickrPress.php');
