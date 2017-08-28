3e48fc6ec2100e0df984e8818a4f7218f9075097
fukata <tatsuya.fukata@gmail.com>
2017-08-28 14:52:54 +0700

    change permission to `write`

--- flickr_oauth.php
+++ flickr_oauth.php
@@ -1,5 +1,5 @@
 <?php
 require_once(dirname(__FILE__).'/check-oauth.php');
 
-FlickrPress::getClient()->getRequestToken(admin_url('admin.php?action=wpfp_flickr_oauth_callback'));
+FlickrPress::getClient()->getRequestToken(admin_url('admin.php?action=wpfp_flickr_oauth_callback'), 'write');
 ?>
